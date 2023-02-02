7c8733f38f5c3d77343e56edc84565595d626e10
noataso <noa740740@outlook.jp>
2023-02-02 23:28:47 +0900

    first commit

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData=[
+    {
+        id:1,
+        name:"faokfoa",
+        content:"fkakfakf",
+    },
+    {
+        id:2,
+        name:"ieinvk",
+        content:"lfpsoo",
+    },
+    {
+        id:3,
+        name:"fpspke",
+        content:"qfdjo",
+    },
+    {
+        id:4,
+        name:"moehap",
+        content:"kodfojhhe",
+    },
+]
